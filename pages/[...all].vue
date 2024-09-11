539b46c86034f3510b1df0cbfacb641cad98b30d
Przemysław Hernik <przemek.hernik@coditive.com>
2024-09-11 10:01:01 +0200

    modify all template

--- pages/[...all].vue
+++ pages/[...all].vue
@@ -1,9 +1,6 @@
 <template>
-  <main
-    p="x4 y10"
-    text="center teal-700 dark:gray-200"
-    class="h-full"
-  >
+  <main>
     <EmptySimple />
+    <!-- <EmptyBackground /> -->
   </main>
 </template>
