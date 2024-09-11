82d214002764ccfbf9b70bf4f06e4f81fcc1a5c0
Przemysław Hernik <przemek.hernik@coditive.com>
2024-09-11 08:48:32 +0200

    add simple 404 component

--- pages/[...all].vue
+++ pages/[...all].vue
@@ -1,28 +1,9 @@
-<script setup lang="ts">
-const router = useRouter()
-</script>
-
 <template>
   <main
     p="x4 y10"
     text="center teal-700 dark:gray-200"
+    class="h-full"
   >
-    <div text-4xl>
-      <Icon
-        name="i-carbon-warning"
-        inline-block
-      />
-    </div>
-    <div>Not found</div>
-    <div>
-      <button
-        text-sm
-        btn
-        m="3 t8"
-        @click="router.back()"
-      >
-        Back
-      </button>
-    </div>
+    <EmptySimple />
   </main>
 </template>
