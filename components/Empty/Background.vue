d3b50b6cbc04ff773cd9bdbb1f8a42bdda80fbcc
Przemysław Hernik <przemek.hernik@coditive.com>
2024-09-13 12:06:24 +0200

    adjust classes

--- components/Empty/Background.vue
+++ components/Empty/Background.vue
@@ -3,7 +3,7 @@
     <img
       src="https://images.unsplash.com/photo-1545972154-9bb223aac798?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3050&q=80&exp=8&con=-15&sat=-75"
       alt=""
-      class="absolute inset-0 -z-10 h-full w-full object-cover object-center"
+      class="absolute inset-0 -z-10 size-full object-cover object-center"
     >
     <div class="mx-auto max-w-7xl px-6 py-32 text-center sm:py-60 lg:px-8">
       <p class="text-base font-semibold leading-8 text-white">
